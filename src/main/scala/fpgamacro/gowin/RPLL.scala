7f968b044611f6e0e59db7768a646572c626ad73
scpcom <scpcom@gmx.de>
2022-03-15 18:59:23 +0100

    gowin: replace gowin_rpll.v with more flexible module by directly using rPLL

---
+++ src/main/scala/fpgamacro/gowin/RPLL.scala
@@ -0,0 +1,87 @@
+package fpgamacro.gowin
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.Param
+
+/* rPLL */
+class rPLL(val pm: Map[String, Param]) extends BlackBox(pm){
+    val io = IO(new Bundle{
+        val CLKOUT = Output(Clock())
+        val LOCK = Output(UInt(1.W))
+        val CLKOUTP = Output(Clock())
+        val CLKOUTD = Output(Clock())
+        val CLKOUTD3 = Output(Clock())
+        val RESET = Input(UInt(1.W))
+        val RESET_P = Input(UInt(1.W))
+        val CLKIN = Input(Clock())
+        val CLKFB = Input(Bool())
+        val FBDSEL = Input(UInt(6.W))
+        val IDSEL = Input(UInt(6.W))
+        val ODSEL = Input(UInt(6.W))
+        val PSDA = Input(UInt(4.W))
+        val DUTYDA = Input(UInt(4.W))
+        val FDLY = Input(UInt(4.W))
+    })
+}
+
+/* Gowin rPLL (GW1NR-9) */
+class Gowin_rPLL extends RawModule {
+    val io = IO(new Bundle {
+        val clkin = Input(Clock())
+        val clkout = Output(Clock())
+        val lock = Output(Bool())
+    })
+
+  val pm: Map[String, Param] = Map(
+  "FCLKIN" -> "27",
+  "DYN_IDIV_SEL" -> "false",
+  "IDIV_SEL" -> 3,
+  "DYN_FBDIV_SEL" -> "false",
+  "FBDIV_SEL" -> 54,
+  "DYN_ODIV_SEL" -> "false",
+  "ODIV_SEL" -> 2,
+  "PSDA_SEL" -> "0000",
+  "DYN_DA_EN" -> "true",
+  "DUTYDA_SEL" -> "1000",
+  "CLKOUT_FT_DIR" -> "1'b1",
+  "CLKOUTP_FT_DIR" -> "1'b1",
+  "CLKOUT_DLY_STEP" -> 0,
+  "CLKOUTP_DLY_STEP" -> 0,
+  "CLKFB_SEL" -> "internal",
+  "CLKOUT_BYPASS" -> "false",
+  "CLKOUTP_BYPASS" -> "false",
+  "CLKOUTD_BYPASS" -> "false",
+  "DYN_SDIV_SEL" -> 2,
+  "CLKOUTD_SRC" -> "CLKOUT",
+  "CLKOUTD3_SRC" -> "CLKOUT",
+  "DEVICE" -> "GW1NR-9")
+
+  val clkoutp_o = Wire(Clock())
+  val clkoutd_o = Wire(Clock())
+  var clkoutd3_o = Wire(Clock())
+  val gw_vcc = Wire(UInt(1.W))
+  val gw_gnd = Wire(UInt(1.W))
+
+  gw_vcc := 1.U(1.W)
+  gw_gnd := 0.U(1.W)
+
+  val rpll_inst = Module(new rPLL(pm))
+
+  io.clkout := rpll_inst.io.CLKOUT
+  io.lock := rpll_inst.io.LOCK
+  clkoutp_o := rpll_inst.io.CLKOUTP
+  clkoutd_o := rpll_inst.io.CLKOUTD
+  clkoutd3_o := rpll_inst.io.CLKOUTD3
+
+  rpll_inst.io.RESET := gw_gnd
+  rpll_inst.io.RESET_P := gw_gnd
+  rpll_inst.io.CLKIN := io.clkin
+  rpll_inst.io.CLKFB := gw_gnd
+  rpll_inst.io.FBDSEL := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+  rpll_inst.io.IDSEL := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+  rpll_inst.io.ODSEL := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+  rpll_inst.io.PSDA := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+  rpll_inst.io.DUTYDA := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+  rpll_inst.io.FDLY := Cat(gw_gnd,gw_gnd,gw_gnd,gw_gnd)
+}
