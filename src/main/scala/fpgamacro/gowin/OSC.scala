e6796c7b0a95b6a2654a5ffc874138141229b54c
scpcom <scpcom@gmx.de>
2022-03-17 03:35:48 +0100

    gowin: add OSC

---
+++ src/main/scala/fpgamacro/gowin/OSC.scala
@@ -0,0 +1,27 @@
+package hdl
+package gowin_osc
+
+import chisel3._
+import chisel3.experimental.Param
+
+/* OSC */
+class OSC(val pm: Map[String, Param]) extends BlackBox(pm){
+    val io = IO(new Bundle{
+        val OSCOUT = Output(Clock())
+    })
+}
+
+/* Gowin OSC */
+class Gowin_OSC(FREQ_DIV: Byte = 10) extends RawModule {
+    val io = IO(new Bundle{
+        val oscout = Output(Clock())
+    })
+
+  val om: Map[String, Param] = Map(
+  "FREQ_DIV" -> FREQ_DIV,
+  "DEVICE" -> "GW1NR-9")
+
+  val osc_inst = Module(new OSC(om))
+
+  io.oscout := osc_inst.io.OSCOUT
+}
